ea1c9a22d8e3e6cdbcddbe1b2cca8be808c37f69
tediko <tediko123@wp.pl>
2020-11-27 12:03:00 +0100

    Added menuToggle module

---
+++ js/menuToggle.js
@@ -0,0 +1,26 @@
+const menuBtn = document.querySelector('[data-hamburger]');
+const menuNav = document.querySelector('[data-nav]');
+const menuItems = document.querySelectorAll('[data-nav-anim]');
+const body = document.querySelector('body');
+
+const toggleMenu = () => {
+    if (menuBtn.classList.contains('open')) {
+        menuBtn.classList.remove('open');
+        menuNav.classList.remove('header__nav--active');
+        body.style.overflowY = 'visible'
+        
+        menuItems.forEach(item => item.style.animation = ``);
+
+    } else {
+        menuBtn.classList.add('open');
+        menuNav.classList.add('header__nav--active');
+        body.style.overflowY = 'hidden';
+
+        menuItems.forEach((item, idx) => {
+            item.style.animation = `nav-anim 500ms ease-in forwards`;
+            item.style.animationDelay = `${idx * 300}ms`;
+        })
+    }
+}
+
+export default menuBtn.addEventListener('click', toggleMenu);

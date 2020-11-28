48fda397d2608306fe805ffbf88b1e45d5255fa9
tediko <tediko123@wp.pl>
2020-11-28 09:28:08 +0100

    Added body.classList add/remove  to toggleMenu

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,17 +1,20 @@
 const menuBtn = document.querySelector('[data-hamburger]');
 const menuNav = document.querySelector('[data-nav]');
 const menuItems = document.querySelectorAll('[data-nav-anim]');
+const body = document.querySelector('body');
 
 const toggleMenu = () => {
     if (menuBtn.classList.contains('open')) {
         menuBtn.classList.remove('open');
         menuNav.classList.remove('header__nav--active');
+        body.classList.remove('scroll-y');
         
         menuItems.forEach(item => item.style.animation = ``);
-
+        
     } else {
         menuBtn.classList.add('open');
         menuNav.classList.add('header__nav--active');
+        body.classList.add('scroll-y');
 
         menuItems.forEach((item, idx) => {
             item.style.animation = `nav-anim 500ms ease-in forwards`;
